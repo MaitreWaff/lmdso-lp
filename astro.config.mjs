79cfd8dd5dfd51a34cc43d3da63cfa43297ccd4d
MaitreWaff <8455859+MaitreWaff@users.noreply.github.com>
2026-02-15 21:10:27 +0100

    First customization.
    Changing the base elements
    Adding UI components

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 const owner = "astrojs";
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lmdso-lp.github.io',
+  // base: '/lmdso-lp',
   integrations: [tailwind()],
 });
